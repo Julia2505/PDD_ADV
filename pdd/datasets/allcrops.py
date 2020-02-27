8bdbe8fe701bb7ad046edb8188d35a43cc7c5e0b
Julia Zudikhina <44220852+Julia2505@users.noreply.github.com>
2020-02-27 16:30:12 +0300

    Update allcrops.py

--- pdd/datasets/allcrops.py
+++ pdd/datasets/allcrops.py
@@ -10,7 +10,7 @@ import numpy as np
 import sys
 
 
-def load_data(path='crops.tar', 
+def load_data('/content/gdrive/My Drive/DIPLOM/crops.tar', 
               split_on_train_test=False, 
               test_size=None, 
               random_state=0):
