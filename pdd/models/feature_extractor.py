53bc63520fa462e68a34e280e02004070e2a5d66
Julia Zudikhina <44220852+Julia2505@users.noreply.github.com>
2020-03-10 11:59:10 +0300

    Update feature_extractor.py

--- pdd/models/feature_extractor.py
+++ pdd/models/feature_extractor.py
@@ -52,8 +52,5 @@ def get_feature_extractor(input_shape):
     x = conv_block(512, (3, 3), batch_norm=True)(x)
     x = Flatten()(x)
     #encoded = Dense(1024, activation='sigmoid')(x)
-    encoded = Dense(units=Int('units_input',    # Полносвязный слой с разным количеством нейронов
-                                   min_value=32,    # минимальное количество нейронов - 128
-                                   max_value=1024,   # максимальное количество - 1024
-                                   step=32), activation=random.choice(['relu', 'sigmoid', 'tanh', 'elu', 'selu']))(x)
+    encoded = Dense(random.randint(512, 1024), activation=random.choice(['relu', 'sigmoid', 'tanh', 'elu', 'selu']))(x)
     return Model(inputs, encoded)
