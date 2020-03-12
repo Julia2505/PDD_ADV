2af336f895fd43dbde9d9ddea551c545dac36573
Julia Zudikhina <44220852+Julia2505@users.noreply.github.com>
2020-03-12 18:23:25 +0300

    Update feature_extractor.py

--- pdd/models/feature_extractor.py
+++ pdd/models/feature_extractor.py
@@ -46,14 +46,14 @@ def conv_block(n_filters,
 
 
 def get_pretrained_feature_extractor(input_shape):
-    #inputs = Input(input_shape)
+    inputs = Input(input_shape)
     #x = conv_block(32, (10, 10), batch_norm=True)(inputs)
     #x = conv_block(64, (7, 7), batch_norm=True)(x)
     #x = conv_block(128, (5, 5), batch_norm=True)(x)
     #x = conv_block(256, (3, 3), batch_norm=True)(x)
     #x = conv_block(512, (3, 3), batch_norm=True)(x)
     #x = Flatten()(x)
-    base_model=MobileNet(input_shape=input_shape, alpha=1.0, depth_multiplier=1, dropout=1e-3, include_top=False, weights='imagenet', input_tensor=None, pooling=None, classes=None)
+    base_model=MobileNet(input_shape=inputs, alpha=1.0, depth_multiplier=1, dropout=1e-3, include_top=False, weights='imagenet', input_tensor=None, pooling=None, classes=None)
     x=base_model.output
     encoded = Dense(1024, activation='sigmoid')(x)
     #encoded = Dense(1024, activation=random.choice(['relu', 'sigmoid', 'tanh', 'elu', 'selu']))(x)
