9abc811c4a13f6e474b40e673f01f9eeb74f04a2
Julia Zudikhina <44220852+Julia2505@users.noreply.github.com>
2020-03-13 13:11:34 +0300

    Update feature_extractor.py

--- pdd/models/feature_extractor.py
+++ pdd/models/feature_extractor.py
@@ -49,7 +49,7 @@ def conv_block(n_filters,
 @tf.function
 def get_pretrained_feature_extractor(input_shape):
     inputs = Input(input_shape)
-    input_tensor =  Input(shape=(256, 256, 3))
+    #input_tensor =  Input(shape=(256, 256, 3))
     base_model=MobileNetV2(input_shape=inputs, alpha=1.0, include_top=False, weights=None, input_tensor=None, pooling=None, classes=None)
     x=base_model.output
     x = Flatten()(x)
